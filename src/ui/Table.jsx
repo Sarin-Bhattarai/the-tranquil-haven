1f7ad2752a376ddc0651ede85f12ca8061a33db4
sarin punk <sarin.bhattarai@icp.edu.np>
2024-10-02 10:47:41 +0545

    feat(add ons)

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -1,5 +1,5 @@
-import { createContext, useContext } from "react";
 import styled from "styled-components";
+import { createContext, useContext } from "react";
 
 const StyledTable = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -90,7 +90,10 @@ function Row({ children }) {
   );
 }
 
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Row = Row;
