c6f22eb7d4eb6ce2ca41d829f4fea8a624c5632c
sarin punk <sarin.bhattarai@icp.edu.np>
2024-10-07 12:49:10 +0545

    feat(filter-addons)

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,13 +2,14 @@ import Row from "../ui/Row";
 import Heading from "../ui/Heading";
 import AddCabin from "../features/cabins/AddCabin";
 import CabinTable from "../features/cabins/CabinTable";
+import CabinTableOperation from "../features/cabins/CabinTableOperation";
 
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperation />
       </Row>
 
       <Row>
