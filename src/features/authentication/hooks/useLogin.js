8fdcf8e5912803d3de3d0fa5e37744985b326f6e
Sarin Bhattarai <sarin.bhattarai.a19@icp.edu.np>
2024-10-29 15:22:00 +0545

    feat(fixing a bug)

--- src/features/authentication/hooks/useLogin.js
+++ src/features/authentication/hooks/useLogin.js
@@ -1,14 +1,16 @@
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import { useMutation } from "@tanstack/react-query";
 import { login as loginApi } from "../../../services/apiAuth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
--- src/features/authentication/hooks/useLogout.js
+++ src/features/authentication/hooks/useLogout.js
@@ -7,7 +7,7 @@ export function useLogout() {
   const queryClient = useQueryClient();
 
   const { mutate: logout, isLoading } = useMutation({
-    mutationFn: logoutApi(),
+    mutationFn: logoutApi,
     onSuccess: () => {
       queryClient.removeQueries();
       navigate("/login", { replace: true });
