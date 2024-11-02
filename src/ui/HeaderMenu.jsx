bd49c188d99ff41b9e636a3d7825211bf8658f0d
Sarin Bhattarai <sarin.bhattarai.a19@icp.edu.np>
2024-11-02 12:24:08 +0545

    feat(header)

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,29 @@
+import icon from "../utils/icon";
+import ButtonIcon from "./ButtonIcon";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import Logout from "../features/authentication/Logout";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <icon.HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
