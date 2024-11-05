213ec8093a632a5b9f5a022693793e2d85a96ba8
Sarin Bhattarai <sarin.bhattarai.a19@icp.edu.np>
2024-11-05 13:03:45 +0545

    feat(Dashboard)

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,18 @@
 import Row from "../ui/Row";
 import Heading from "../ui/Heading";
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+import DashboardFilter from "../features/dashboard/DashboardFilter";
 
 function Dashboard() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashboardFilter />
+      </Row>
+
+      <DashboardLayout />
+    </>
   );
 }
 
